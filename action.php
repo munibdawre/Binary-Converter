2800d185c5309f3dc454f416f05c613609682d24
munibdawre <68640132+munibdawre@users.noreply.github.com>
2020-12-26 11:58:31 +0530

    Add files via upload

---
+++ action.php
@@ -0,0 +1,18 @@
+<html>
+    <head><title>Result</title>
+</head>
+
+    <body>
+        <center>
+        <?php
+
+        $n1= $_POST["num"];
+        echo decbin("$n1");
+        echo"<br>";
+        echo"<br>";
+    
+        ?>
+        <a href="dec.html">Back</a>
+    </center>
+</body>
+</html>
